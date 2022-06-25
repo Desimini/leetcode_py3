b2a8508a15d5f4e3e47d13303dd70be05fb2da65
Dabist <81484377+Dabist@users.noreply.github.com>
2022-06-25 13:48:29 +0200

    Update strategy and description

--- n_steps_to_0.py
+++ n_steps_to_0.py
@@ -3,6 +3,16 @@ class Solution:
     Runtime: 24 ms, faster than 98.87% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.
     Memory Usage: 13.8 MB, less than 95.75% of Python3 online submissions for Number of Steps to Reduce a Number to Zero."""
     def numberOfSteps(self, num: int) -> int:
+        """
+                :param num
+                :return:
+                Given an integer num, return the number of steps to reduce it to zero.
+                In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract
+                1 from it.
+
+                Strategy: check the remainder of the division by two to decide the operation to apply and increase the
+                depth parameter to keep track of the amount of steps
+                """
         depth = 0
         while True:
             if not num:
